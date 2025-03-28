f7eb6dfb4b0095a59d7f22c9fb9966bc072e5e01
PybricksMyPersonalAccount <cuentademogithub@hotmail.com>
2025-03-28 10:29:18 -0300

    Create first_program.py
    Programa que trabajamos en la primera clase referente a motores

---
+++ first_program.py
@@ -0,0 +1,11 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch, time
+from pybricks import version
+
+hub = PrimeHub()
+
+print('¡Hola pybricks!')
+print(version)
