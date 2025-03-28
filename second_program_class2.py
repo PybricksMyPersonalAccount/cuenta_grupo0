aa4d2ff3d9649955dfce613fc892e85e938db452
PybricksMyPersonalAccount <cuentademogithub@hotmail.com>
2025-03-28 10:52:11 -0300

    "Segunda clase de motores"

---
+++ second_program_class2.py
@@ -0,0 +1,10 @@
+from pybricks.hubs import PrimeHub
+from pybricks.pupdevices import Motor, ColorSensor, UltrasonicSensor, ForceSensor
+from pybricks.parameters import Button, Color, Direction, Port, Side, Stop
+from pybricks.robotics import DriveBase
+from pybricks.tools import wait, StopWatch
+
+hub = PrimeHub()
+
+print('Esto es archivo relacionado a una nueva clase de Python')
+print('Esto es una segunda línea')
